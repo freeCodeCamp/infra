e0298a5f15b99f8c2f131da61fae6de746fb2c96
Mrugesh Mohapatra <hi@mrugesh.dev>
2021-12-10 14:57:17 +0530

    fix(cdktf): add commands to npm run-script, update code

--- live/stg/main.ts
+++ live/stg/main.ts
@@ -2,7 +2,12 @@ import { Construct } from 'constructs';
 import { App, RemoteBackend, TerraformStack } from 'cdktf';
 
 // import * as keys from './config/env';
-import { AzurermProvider, ResourceGroup } from '@cdktf/provider-azurerm';
+import {
+  AzurermProvider
+  // ResourceGroup,
+  // Subnet,
+  // VirtualNetwork
+} from '@cdktf/provider-azurerm';
 class StgStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
@@ -17,10 +22,28 @@ class StgStack extends TerraformStack {
     // Resource Group
     // ----------------------------------
 
-    new ResourceGroup(this, 'stg_rg', {
-      name: 'stg_rg',
-      location: 'westus'
-    });
+    // const stg_rg = new ResourceGroup(this, 'stg_rg', {
+    //   name: 'stg_rg',
+    //   location: 'westus'
+    // });
+
+    // // ----------------------------------
+    // // Virtual Network
+    // // ----------------------------------
+
+    // const stg_vnet = new VirtualNetwork(this, 'stg_vnet', {
+    //   name: 'stg_vnet',
+    //   resourceGroupName: stg_rg.name,
+    //   location: 'westus',
+    //   addressSpace: ['10.0.0.0/8']
+    // });
+
+    // const stg_subnet = new Subnet(this, 'stg_subnet', {
+    //   name: 'stg_subnet',
+    //   resourceGroupName: stg_rg.name,
+    //   virtualNetworkName: stg_vnet.name,
+    //   addressPrefixes: ['10.240.0.0/16']
+    // });
   }
 }
 
