d311148bbe05c18034136ecbd5505286aeab9e89
Mrugesh Mohapatra <hi@mrugesh.dev>
2021-12-24 10:34:02 +0530

    refactor: warnings for missing keys

--- live/stg/config/env.ts
+++ live/stg/config/env.ts
@@ -1,18 +1,21 @@
-import { config } from 'dotenv';
 import { resolve } from 'path';
-import { existsSync } from 'fs';
 
 const envPath = resolve(__dirname, '../.env');
-
-if (!existsSync(envPath)) {
-  throw Error(`Could not locate environment variables file.`);
+const { error } = require('dotenv').config({ path: envPath });
+if (error) {
+  console.warn(`
+  ----------------------------------------------------
+  Warning: .env file not found.
+  ----------------------------------------------------
+  Please copy sample.env to .env
+  You can ignore this warning if using a different way
+  to setup this environment.
+  ----------------------------------------------------
+  `);
 }
-config({ path: envPath });
 
-const {
-  SSH_PUBLIC_KEY: sshPublicKey,
-  BASE64_ENCODED_CUSTOM_DATA: customData
-} = process.env;
+const { SSH_PUBLIC_KEY: sshPublicKey, BASE64_ENCODED_CUSTOM_DATA: customData } =
+  process.env;
 
 export const ssh_public_key: string = String(sshPublicKey).toString();
 export const custom_data: string = String(customData).toString();
