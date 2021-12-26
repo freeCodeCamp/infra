6f2dce9af567951e2c0e7a8daa15e73c0e53f4c4
Mrugesh Mohapatra <hi@mrugesh.dev>
2021-12-26 11:05:55 +0530

    feat(cdktf): add common resources

--- stacks/main.ts
+++ stacks/main.ts
@@ -1,19 +1,22 @@
 import { App, RemoteBackend } from 'cdktf';
 
-import prdMySQLDBStack from './prd/mysql-fs/';
-import opsRGMachineImagesStack from './prd/resource-group/ops-rg-machine-images';
+import prdMySQLDBStack from './prd/instances-mysql-flexible-server';
+import opsRGMachineImagesStack from './prd/instances-resource-group/ops-rg-machine-images';
+import opsRGCommonStack from './prd/instances-resource-group/ops-rg-common';
 
 const app = new App();
 
-const _prdMySQLDBStack = new prdMySQLDBStack(app, 'prd-stack-mysql-db');
-new RemoteBackend(_prdMySQLDBStack, {
+// Operations - Machine Images
+const _opsRGCommonStack = new opsRGCommonStack(app, 'ops-stack-common');
+new RemoteBackend(_opsRGCommonStack, {
   hostname: 'app.terraform.io',
   organization: 'freecodecamp',
   workspaces: {
-    name: 'tfws-prd-stack-mysql-db'
+    name: 'tfws-ops-stack-common'
   }
 });
 
+// Operations - Machine Images
 const _opsRGMachineImagesStack = new opsRGMachineImagesStack(
   app,
   'ops-stack-machine-images'
@@ -23,6 +26,16 @@ new RemoteBackend(_opsRGMachineImagesStack, {
   organization: 'freecodecamp',
   workspaces: {
     name: 'tfws-ops-stack-machine-images'
+  }
+});
+
+// Production - MySQL DB (Flexible Server) DB Cluster
+const _prdMySQLDBStack = new prdMySQLDBStack(app, 'prd-stack-mysql-db');
+new RemoteBackend(_prdMySQLDBStack, {
+  hostname: 'app.terraform.io',
+  organization: 'freecodecamp',
+  workspaces: {
+    name: 'tfws-prd-stack-mysql-db'
   }
 });
 
