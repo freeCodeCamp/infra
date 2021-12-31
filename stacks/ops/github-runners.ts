2aea0fa2108c1f133a821e4137861e70e21630e3
Mrugesh Mohapatra <hi@mrugesh.dev>
2021-12-31 09:46:00 +0530

    feat(infra): add github runners to Azure

---
+++ stacks/ops/github-runners.ts
@@ -0,0 +1,60 @@
+import { Construct } from 'constructs';
+import { TerraformStack } from 'cdktf';
+import {
+  AzurermProvider,
+  ResourceGroup,
+  Subnet,
+  VirtualNetwork
+} from '@cdktf/provider-azurerm';
+
+import { createVirtualMachine } from '../components/virtual-machine';
+import { createAzureRBACServicePrincipal } from './../config/service_principal';
+
+export default class GitHubRunners extends TerraformStack {
+  constructor(scope: Construct, name: string, config: any) {
+    super(scope, name);
+
+    const { env } = config;
+
+    const { subscriptionId, tenantId, clientId, clientSecret } =
+      createAzureRBACServicePrincipal(this);
+    new AzurermProvider(this, 'azurerm', {
+      features: {},
+      subscriptionId: subscriptionId.stringValue,
+      tenantId: tenantId.stringValue,
+      clientId: clientId.stringValue,
+      clientSecret: clientSecret.stringValue
+    });
+
+    const rgIdentifier = `${env}-rg-${name}`;
+    const rg = new ResourceGroup(this, rgIdentifier, {
+      name: rgIdentifier,
+      location: 'eastus2'
+    });
+
+    const vnetIdentifier = `${env}-vnet-${name}`;
+    const vnet = new VirtualNetwork(this, vnetIdentifier, {
+      name: vnetIdentifier,
+      resourceGroupName: rg.name,
+      location: rg.location,
+      addressSpace: ['172.16.0.0/16']
+    });
+
+    const subnetIdentifier = `${env}-subnet-${name}`;
+    const subnet = new Subnet(this, subnetIdentifier, {
+      name: subnetIdentifier,
+      resourceGroupName: rg.name,
+      virtualNetworkName: vnet.name,
+      addressPrefixes: ['172.16.0.0/24']
+    });
+
+    createVirtualMachine(this, {
+      stackName: name,
+      vmName: 'engnews',
+      rg: rg,
+      env: env,
+      subnet: subnet,
+      privateIP: '172.16.0.10'
+    });
+  }
+}
