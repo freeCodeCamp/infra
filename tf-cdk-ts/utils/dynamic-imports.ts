b192f064b084efdfad12368b73d9ee866f6a5c4a
Mrugesh Mohapatra <hi@mrugesh.dev>
2022-06-21 19:37:15 +0530

    fix: remove uneeded utils

--- tf-cdk-ts/utils/dynamic-imports.ts
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/a/70192405
-
-// eslint-disable-next-line no-new-func
-const importDynamic = new Function('modulePath', 'return import(modulePath)');
-
-export const fetch = async (...args: any[]) => {
-  const module = await importDynamic('node-fetch');
-  return module.default(...args);
-};
