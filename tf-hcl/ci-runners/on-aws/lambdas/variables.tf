557b95fd83717d52b66080da799afaafa43ad807
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2023-04-02 18:31:19 +0530

    chore: remove CI runners from the infra repository (#434)

--- tf-hcl/ci-runners/on-aws/lambdas/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "download_lambda_tag" {
-  description = "The tag of the lambda to download"
-  type        = string
-}
--- tf-hcl/ci-runners/on-aws/vpc.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-  source = "git::https://github.com/philips-software/terraform-aws-vpc.git?ref=2.2.0"
-
-  environment                = var.environment
-  aws_region                 = var.aws_region
-  create_private_hosted_zone = false
-}
