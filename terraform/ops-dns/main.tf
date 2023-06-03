6ffbb354235ff0a7d9556f3e69ad5bbcd51b81a0
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-06-03 23:31:45 +0530

    feat: add a local IP record

--- terraform/ops-dns/main.tf
+++ terraform/ops-dns/main.tf
@@ -13,8 +13,22 @@ provider "linode" {
   token = var.linode_token
 }
 
+# DO NOT DELETE OR MODIFY THIS RESOURCE --------
+#
+# This resource is a Domain resource that may have records
+# outside of this module or even outside of Terraform.
 resource "linode_domain" "freecodecamp_net" {
   domain    = "freecodecamp.net"
   type      = "master"
   soa_email = "dev@freecodecamp.org"
+}
+# DO NOT DELETE OR MODIFY THIS RESOURCE --------
+
+resource "linode_domain_record" "local" {
+  domain_id   = linode_domain.freecodecamp_net.id
+  name        = "local"
+  record_type = "A"
+  target      = "127.0.0.1"
+
+  ttl_sec = 3600
 }
