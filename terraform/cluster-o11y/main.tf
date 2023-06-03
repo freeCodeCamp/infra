430ca808fc8a12db3a61a2983bad4482534b067c
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-06-03 14:29:08 +0530

    fix(o11y): update dns records

--- terraform/cluster-o11y/main.tf
+++ terraform/cluster-o11y/main.tf
@@ -51,7 +51,7 @@ resource "linode_domain_record" "ops_o11y_leaders_records" {
   count = var.leader_node_count
 
   domain_id   = data.linode_domain.ops_dns_domain.id
-  name        = "o11y-ldr-${count.index + 1}.o11y.dns.${data.linode_domain.ops_dns_domain.domain}"
+  name        = "ldr-${count.index + 1}.o11y.${data.linode_domain.ops_dns_domain.domain}"
   record_type = "A"
   target      = linode_instance.ops_o11y_leaders[count.index].ip_address
   ttl_sec     = 60
@@ -94,7 +94,7 @@ resource "linode_domain_record" "ops_o11y_workers_records" {
   count = var.worker_node_count
 
   domain_id   = data.linode_domain.ops_dns_domain.id
-  name        = "o11y-wkr-${count.index + 1}.o11y.dns.${data.linode_domain.ops_dns_domain.domain}"
+  name        = "wkr-${count.index + 1}.o11y.${data.linode_domain.ops_dns_domain.domain}"
   record_type = "A"
   target      = linode_instance.ops_o11y_workers[count.index].ip_address
   ttl_sec     = 60
