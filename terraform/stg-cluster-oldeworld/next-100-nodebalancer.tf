4083b5205ab90196839630487e167f1e87ab6679
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2023-07-11 01:58:29 +0530

    Update terraform/stg-cluster-oldeworld/next-100-nodebalancer.tf

--- terraform/stg-cluster-oldeworld/next-100-nodebalancer.tf
+++ terraform/stg-cluster-oldeworld/next-100-nodebalancer.tf
@@ -1,7 +1,7 @@
 resource "linode_nodebalancer" "stg_oldeworld_nb_pxy" {
   region = var.region
   label  = "stg-nb-oldeworld-pxy"
-  tags   = ["stg", "oldeworld", "pxy"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
+  tags   = ["stg", "oldeworld", "nb_pxy"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
 }
 
 resource "linode_nodebalancer_config" "stg_oldeworld_nb_pxy_config__port_80" {
