973873668243bb84ad2ca7ac69ef91096a19775c
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-07-18 03:00:51 +0530

    feat(terraform): update IDs for the NB for the prd cluster

--- terraform/prd-cluster-oldeworld/next-02-nginx-origins.tf
+++ terraform/prd-cluster-oldeworld/next-02-nginx-origins.tf
@@ -1,14 +1,14 @@
 data "linode_nodebalancer" "prd_oldeworld_nb_pxy" {
-  id = 000000 # TODO: Find a way to get this ID dynamically
+  id = 389361 # TODO: Find a way to get this ID dynamically
 }
 
 data "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_config__port_443" {
-  id              = 000000 # TODO: Find a way to get this ID dynamically
+  id              = 598263 # TODO: Find a way to get this ID dynamically
   nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy.id
 }
 
 data "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_config__port_80" {
-  id              = 000000 # TODO: Find a way to get this ID dynamically
+  id              = 598264 # TODO: Find a way to get this ID dynamically
   nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy.id
 }
 
