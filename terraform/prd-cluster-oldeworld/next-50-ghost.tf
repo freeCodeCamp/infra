1caecc1a59aef4ad82ad409b2fd6f05a9160af4b
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2023-07-22 03:31:54 +0530

    feat(terraform): add ghost instances for prd (#566)

--- terraform/prd-cluster-oldeworld/next-50-ghost.tf
+++ terraform/prd-cluster-oldeworld/next-50-ghost.tf
@@ -1,18 +1,20 @@
-resource "linode_instance" "prd_oldeworld_newstst" {
-  label  = "prd-vm-oldeworld-newstst-1"
-  group  = "oldeworld_newstst" # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
-  region = var.region
-  type   = "g6-standard-2"
+resource "linode_instance" "prd_oldeworld_news" {
+  for_each = local.ghost_instances
+  label    = "prd-vm-oldeworld-news-${each.value.name}"
+  group    = "oldeworld_news" # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
+  region   = var.region
+  type     = "g6-standard-2"
 
   private_ip = true
 
-  tags = ["prd", "oldeworld", "prd_oldeworld_news", "news", "tst"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
+  tags = ["prd", "oldeworld", "prd_oldeworld_news", "news", "${each.value.name}"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
 }
 
-resource "linode_instance_disk" "prd_oldeworld_newstst_disk__boot" {
-  label     = "prd-vm-oldeworld-newstst-1-boot"
-  linode_id = linode_instance.prd_oldeworld_newstst.id
-  size      = linode_instance.prd_oldeworld_newstst.specs.0.disk
+resource "linode_instance_disk" "prd_oldeworld_news_disk__boot" {
+  for_each  = local.ghost_instances
+  label     = "prd-vm-oldeworld-news-${each.value.name}-boot"
+  linode_id = linode_instance.prd_oldeworld_news[each.key].id
+  size      = linode_instance.prd_oldeworld_news[each.key].specs.0.disk
 
   image     = data.hcp_packer_image.linode_ubuntu.cloud_image_id
   root_pass = var.password
@@ -21,19 +23,30 @@ resource "linode_instance_disk" "prd_oldeworld_newstst_disk__boot" {
   stackscript_data = {
     userdata = base64encode(
       templatefile("${path.root}/cloud-init--userdata.yml.tftpl", {
-        tf_hostname = "newstst-1.oldeworld.prd.${data.linode_domain.ops_dns_domain.domain}"
+        tf_hostname = "news-${each.value.name}.oldeworld.prd.${data.linode_domain.ops_dns_domain.domain}"
       })
     )
   }
 }
 
-resource "linode_instance_config" "prd_oldeworld_newstst_config" {
-  label     = "prd-vm-oldeworld-newstst-config"
-  linode_id = linode_instance.prd_oldeworld_newstst.id
+resource "linode_volume" "prd_oldeworld_news_volume__data" {
+  for_each = local.ghost_instances
+  label    = "prd-vm-oldeworld-news-${each.value.name}-data"
+  size     = 120
+  region   = var.region
+}
+
+resource "linode_instance_config" "prd_oldeworld_news_config" {
+  for_each  = local.ghost_instances
+  label     = "prd-vm-oldeworld-news-config"
+  linode_id = linode_instance.prd_oldeworld_news[each.key].id
 
   devices {
     sda {
-      disk_id = linode_instance_disk.prd_oldeworld_newstst_disk__boot.id
+      disk_id = linode_instance_disk.prd_oldeworld_news_disk__boot[each.key].id
+    }
+    sdb {
+      volume_id = linode_volume.prd_oldeworld_news_volume__data[each.key].id
     }
   }
 
@@ -47,14 +60,14 @@ resource "linode_instance_config" "prd_oldeworld_newstst_config" {
     purpose = "vlan"
     label   = "oldeworld-vlan"
     # This results in IPAM address like 10.0.0.11/24, 10.0.0.12/24, etc.
-    ipam_address = "${cidrhost("10.0.0.0/8", local.ipam_block_newstst + 1)}/24"
+    ipam_address = "${cidrhost("10.0.0.0/8", tonumber(local.ipam_block_news + each.value.ipam_id))}/24"
   }
 
   connection {
     type     = "ssh"
     user     = "root"
     password = var.password
-    host     = linode_instance.prd_oldeworld_newstst.ip_address
+    host     = linode_instance.prd_oldeworld_news[each.key].ip_address
   }
 
   # All of the provisioning should be done via cloud-init.
@@ -83,26 +96,32 @@ resource "linode_instance_config" "prd_oldeworld_newstst_config" {
   booted = true
 }
 
-resource "linode_domain_record" "prd_oldeworld_newstst_dnsrecord__vlan" {
+resource "linode_domain_record" "prd_oldeworld_news_dnsrecord__vlan" {
+  for_each = local.ghost_instances
+
   domain_id   = data.linode_domain.ops_dns_domain.id
-  name        = "newstst-1.oldeworld.prd"
+  name        = "news-${each.value.name}.oldeworld.prd"
   record_type = "A"
-  target      = trimsuffix(linode_instance_config.prd_oldeworld_newstst_config.interface[1].ipam_address, "/24")
+  target      = trimsuffix(linode_instance_config.prd_oldeworld_news_config[each.key].interface[1].ipam_address, "/24")
   ttl_sec     = 120
 }
 
-resource "linode_domain_record" "prd_oldeworld_newstst_dnsrecord__public" {
+resource "linode_domain_record" "prd_oldeworld_news_dnsrecord__public" {
+  for_each = local.ghost_instances
+
   domain_id   = data.linode_domain.ops_dns_domain.id
-  name        = "pub.newstst-1.oldeworld.prd.${var.network_subdomain}"
+  name        = "pub.news-${each.value.name}.oldeworld.prd.${var.network_subdomain}"
   record_type = "A"
-  target      = linode_instance.prd_oldeworld_newstst.ip_address
+  target      = linode_instance.prd_oldeworld_news[each.key].ip_address
   ttl_sec     = 120
 }
 
-resource "linode_domain_record" "prd_oldeworld_newstst_dnsrecord__private" {
+resource "linode_domain_record" "prd_oldeworld_news_dnsrecord__private" {
+  for_each = local.ghost_instances
+
   domain_id   = data.linode_domain.ops_dns_domain.id
-  name        = "prv.newstst-1.oldeworld.prd"
+  name        = "prv.news-${each.value.name}.oldeworld.prd"
   record_type = "A"
-  target      = linode_instance.prd_oldeworld_newstst.private_ip_address
+  target      = linode_instance.prd_oldeworld_news[each.key].private_ip_address
   ttl_sec     = 120
 }
