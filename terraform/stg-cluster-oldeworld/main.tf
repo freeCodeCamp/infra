3f8b4ec10b40921736b6a113a7580ef639723476
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-07-25 20:06:59 +0530

    fix(terraform): drop unused news instances

--- terraform/stg-cluster-oldeworld/main.tf
+++ terraform/stg-cluster-oldeworld/main.tf
@@ -41,13 +41,13 @@ locals {
 locals {
   nws_instances = {
     eng = { name = "eng", ipam_id = 0 }, # 10.0.0.100
-    chn = { name = "chn", ipam_id = 1 }, # 10.0.0.101
-    esp = { name = "esp", ipam_id = 2 }, # ...
-    ita = { name = "ita", ipam_id = 3 },
-    jpn = { name = "jpn", ipam_id = 4 },
-    kor = { name = "kor", ipam_id = 5 },
-    por = { name = "por", ipam_id = 6 },
-    ukr = { name = "ukr", ipam_id = 7 },
+    # chn = { name = "chn", ipam_id = 1 }, # 10.0.0.101
+    # esp = { name = "esp", ipam_id = 2 }, # ...
+    # ita = { name = "ita", ipam_id = 3 },
+    # jpn = { name = "jpn", ipam_id = 4 },
+    # kor = { name = "kor", ipam_id = 5 },
+    # por = { name = "por", ipam_id = 6 },
+    # ukr = { name = "ukr", ipam_id = 7 },
     # ger = { name = "ger", ipam_id = 8 }
   }
 
