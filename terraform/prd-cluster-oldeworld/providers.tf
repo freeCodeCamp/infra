9929e6b3c6623a8c186effd0959e7540ecdd64df
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2023-09-02 00:10:09 +0530

    feat(terraform): add edgedns for prd oldeworld (#590)

--- terraform/prd-cluster-oldeworld/providers.tf
+++ terraform/prd-cluster-oldeworld/providers.tf
@@ -6,3 +6,8 @@ provider "hcp" {
   client_id     = var.hcp_client_id
   client_secret = var.hcp_client_secret
 }
+
+provider "akamai" {
+  edgerc         = ".edgerc"
+  config_section = "default"
+}
