cf671ea72b682c4a6393ba13c2ef9b93762831a4
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-09-03 18:36:08 +0530

    feat(tailscale): improve scripts to remove hosts

--- ansible/scripts/rm-tailscale-hosts.sh
+++ ansible/scripts/rm-tailscale-hosts.sh
@@ -5,22 +5,41 @@ remove_before_date="$2"
 apikey="$3"
 doit="$4"
 
-echo "Removing hosts from tailnet $tailnet that have not been seen since $remove_before_date"
-echo "Doit is set to: $doit"
+if [[ -z "$apikey" ]]; then
+  echo "Tailscale API key is required"
+  exit 1
+fi
+
+echo "Fetching hosts from tailnet $tailnet that have not been seen since $remove_before_date"
 echo ""
 echo "DANGER: This script will remove hosts from the backend. If you want to do this, pass the 'doit' param."
 echo ""
+echo "Doit is set to: $doit"
 echo ""
-curl -s "https://api.tailscale.com/api/v2/tailnet/$tailnet/devices" -u "$apikey:" | jq -r '.devices[] |  "\(.lastSeen) \(.id) \(.hostname)"' |
-  while read -r seen id hostname; do
-    if [[ $seen < $remove_before_date ]]; then
-      echo Hostname: $hostname - ID: $id " was last seen " $seen " deleting it from the backend"
-      if [[ $doit == "doit" ]]; then
-        curl -s -X DELETE "https://api.tailscale.com/api/v2/device/$id" -u "$apikey:"
-      fi
-    else
-      echo Hostname: $hostname - ID: $id " was last seen " $seen " keeping it"
+
+echo "Getting hosts from tailnet $tailnet..."
+rm -f /tmp/tailscale-hosts
+curl -s "https://api.tailscale.com/api/v2/tailnet/$tailnet/devices" -u "$apikey:" | jq -r '.devices[] | "\(.lastSeen) \(.id) \(.hostname)"' >/tmp/tailscale-hosts
+
+echo ""
+echo "Preserved:"
+while IFS=' ' read -r seen id hostname; do
+  if [[ "$remove_before_date" < "$seen" ]]; then
+    echo "Hostname: $hostname - ID: $id was last seen $seen"
+  fi
+done </tmp/tailscale-hosts
+
+echo ""
+echo ""
+echo ""
+echo "Removed:"
+while IFS=' ' read -r seen id hostname; do
+  if [[ "$remove_before_date" > "$seen" ]]; then
+    echo "Hostname: $hostname - ID: $id was last seen $seen"
+    if [[ $doit == "doit" ]]; then
+      curl -s -X DELETE "https://api.tailscale.com/api/v2/device/$id" -u "$apikey:"
     fi
-  done
+  fi
+done </tmp/tailscale-hosts
 echo ""
 echo "Done"
