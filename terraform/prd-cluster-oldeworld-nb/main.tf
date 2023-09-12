672ba77c6183078ab24c96b94cab45bb192b1d82
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-09-13 00:28:57 +0530

    fix(prd): add additional nodebalancers

--- terraform/prd-cluster-oldeworld-nb/main.tf
+++ terraform/prd-cluster-oldeworld-nb/main.tf
@@ -40,3 +40,31 @@ resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_config__port_443" {
   check_timeout   = 5
   check_attempts  = 3
 }
+
+resource "linode_nodebalancer" "prd_oldeworld_nb_pxy_2" {
+  region = var.region
+  label  = "prd-nb-oldeworld-pxy-2"
+  tags   = ["prd", "oldeworld", "nb_pxy"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
+}
+
+resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_2_config__port_80" {
+  nodebalancer_id = linode_nodebalancer.prd_oldeworld_nb_pxy_2.id
+  port            = 80
+  protocol        = "tcp"
+  algorithm       = "leastconn"
+  check           = "connection"
+  check_interval  = 10
+  check_timeout   = 5
+  check_attempts  = 3
+}
+
+resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_2_config__port_443" {
+  nodebalancer_id = linode_nodebalancer.prd_oldeworld_nb_pxy_2.id
+  port            = 443
+  protocol        = "tcp"
+  algorithm       = "leastconn"
+  check           = "connection"
+  check_interval  = 10
+  check_timeout   = 5
+  check_attempts  = 3
+}
