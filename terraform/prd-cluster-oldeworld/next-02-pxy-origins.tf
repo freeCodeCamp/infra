d3c9a543fea626970aad3b7905960d89dfc1cba6
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-09-14 03:19:36 +0530

    feat(prd,oldeworld): add backends to Nodebalancer

--- terraform/prd-cluster-oldeworld/next-02-pxy-origins.tf
+++ terraform/prd-cluster-oldeworld/next-02-pxy-origins.tf
@@ -61,3 +61,35 @@ resource "linode_nodebalancer_node" "prd_oldeworld_nb_pxy_2_nodes__port_80" {
   address         = "${linode_instance.prd_oldeworld_pxy[count.index].private_ip_address}:80"
   label           = "prd-node-pxy-80-${count.index}"
 }
+
+data "linode_nodebalancer" "prd_oldeworld_nb_pxy_3" {
+  id = 441410
+}
+
+data "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_3_config__port_443" {
+  id              = 670873
+  nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+}
+
+data "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_3_config__port_80" {
+  id              = 670874
+  nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+}
+
+resource "linode_nodebalancer_node" "prd_oldeworld_nb_pxy_3_nodes__port_443" {
+  count = local.pxy_node_count
+
+  nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+  config_id       = data.linode_nodebalancer_config.prd_oldeworld_nb_pxy_3_config__port_443.id
+  address         = "${linode_instance.prd_oldeworld_pxy[count.index].private_ip_address}:443"
+  label           = "prd-node-pxy-443-${count.index}"
+}
+
+resource "linode_nodebalancer_node" "prd_oldeworld_nb_pxy_3_nodes__port_80" {
+  count = local.pxy_node_count
+
+  nodebalancer_id = data.linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+  config_id       = data.linode_nodebalancer_config.prd_oldeworld_nb_pxy_3_config__port_80.id
+  address         = "${linode_instance.prd_oldeworld_pxy[count.index].private_ip_address}:80"
+  label           = "prd-node-pxy-80-${count.index}"
+}
