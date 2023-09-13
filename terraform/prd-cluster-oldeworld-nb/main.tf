61cf443c4c1dcb65f6ecacb770caba4dac3f018c
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-09-14 03:03:10 +0530

    feat(prd,oldeworld): add a third Nodebalancer

--- terraform/prd-cluster-oldeworld-nb/main.tf
+++ terraform/prd-cluster-oldeworld-nb/main.tf
@@ -68,3 +68,31 @@ resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_2_config__port_443" 
   check_timeout   = 5
   check_attempts  = 3
 }
+
+resource "linode_nodebalancer" "prd_oldeworld_nb_pxy_3" {
+  region = var.region
+  label  = "prd-nb-oldeworld-pxy-3"
+  tags   = ["prd", "oldeworld", "nb_pxy"] # Value should use '_' as sepratator for compatibility with Ansible Dynamic Inventory
+}
+
+resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_3_config__port_80" {
+  nodebalancer_id = linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+  port            = 80
+  protocol        = "tcp"
+  algorithm       = "leastconn"
+  check           = "connection"
+  check_interval  = 10
+  check_timeout   = 5
+  check_attempts  = 3
+}
+
+resource "linode_nodebalancer_config" "prd_oldeworld_nb_pxy_3_config__port_443" {
+  nodebalancer_id = linode_nodebalancer.prd_oldeworld_nb_pxy_3.id
+  port            = 443
+  protocol        = "tcp"
+  algorithm       = "leastconn"
+  check           = "connection"
+  check_interval  = 10
+  check_timeout   = 5
+  check_attempts  = 3
+}
