c76f9024775681a7dbdadef6a8d7a7dced098c13
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-02-05 15:54:32 +0530

    fix(ansible): tailscale removal scripts

--- ansible/scripts/get-iso-date.sh
+++ ansible/scripts/get-iso-date.sh
@@ -2,44 +2,89 @@
 input="$1"
 os=$(uname)
 
-if [[ $os == "Linux" ]]; then
-  date --iso-8601=seconds -d "$input"
-elif [[ $os == "Darwin" ]]; then
-  case "$input" in
+# Function to convert time expressions to UTC ISO 8601 format on Linux
+convert_to_utc_linux() {
+  case "$1" in
   *minute* | *minutes* | *min* | *mins*)
-    amount=${input%% minute*}
-    amount=${amount%% minutes*}
-    amount=${amount%% min*}
-    amount=${amount%% mins*}
+    amount=${1%%minute*}
+    amount=${amount%%minutes*}
+    amount=${amount%%min*}
+    amount=${amount%%mins*}
+    date --iso-8601=seconds -u -d "-$amount minutes"
+    ;;
+  *hour* | *hours*)
+    amount=${1%%hour*}
+    amount=${amount%%hours*}
+    date --iso-8601=seconds -u -d "-$amount hours"
+    ;;
+  *day* | *days*)
+    amount=${1%%day*}
+    amount=${amount%%days*}
+    date --iso-8601=seconds -u -d "-$amount days"
+    ;;
+  *week* | *weeks*)
+    amount=${1%%week*}
+    amount=${amount%%weeks*}
+    days=$((amount * 7))
+    date --iso-8601=seconds -u -d "-$days days"
+    ;;
+  *month* | *months*)
+    amount=${1%%month*}
+    amount=${amount%%months*}
+    date --iso-8601=seconds -u -d "-$amount months"
+    ;;
+  *year* | *years*)
+    amount=${1%%year*}
+    amount=${amount%%years*}
+    date --iso-8601=seconds -u -d "-$amount years"
+    ;;
+  esac
+}
+
+# Function to convert time expressions to UTC ISO 8601 format on macOS
+convert_to_utc_darwin() {
+  case "$1" in
+  *minute* | *minutes* | *min* | *mins*)
+    amount=${1%%minute*}
+    amount=${amount%%minutes*}
+    amount=${amount%%min*}
+    amount=${amount%%mins*}
     date -v-"$amount"M -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   *hour* | *hours*)
-    amount=${input%% hour*}
-    amount=${amount%% hours*}
+    amount=${1%%hour*}
+    amount=${amount%%hours*}
     date -v-"$amount"H -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   *day* | *days*)
-    amount=${input%% day*}
-    amount=${amount%% days*}
+    amount=${1%%day*}
+    amount=${amount%%days*}
     date -v-"$amount"d -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   *week* | *weeks*)
-    amount=${input%% week*}
-    amount=${amount%% weeks*}
+    amount=${1%%week*}
+    amount=${amount%%weeks*}
     days=$((amount * 7))
     date -v-"$days"d -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   *month* | *months*)
-    amount=${input%% month*}
-    amount=${amount%% months*}
+    amount=${1%%month*}
+    amount=${amount%%months*}
     date -v-"$amount"m -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   *year* | *years*)
-    amount=${input%% year*}
-    amount=${amount%% years*}
+    amount=${1%%year*}
+    amount=${amount%%years*}
     date -v-"$amount"y -u +"%Y-%m-%dT%H:%M:%SZ"
     ;;
   esac
+}
+
+if [[ $os == "Linux" ]]; then
+  convert_to_utc_linux "$input"
+elif [[ $os == "Darwin" ]]; then
+  convert_to_utc_darwin "$input"
 else
   echo "Unsupported operating system."
+  exit 1
 fi
--- ansible/scripts/rm-tailscale-hosts.sh
+++ ansible/scripts/rm-tailscale-hosts.sh
@@ -10,36 +10,68 @@ if [[ -z "$apikey" ]]; then
   exit 1
 fi
 
-echo "Fetching hosts from tailnet $tailnet that have not been seen since $remove_before_date"
-echo ""
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "DANGER: This script will remove hosts from the backend. If you want to do this, pass the 'doit' param."
-echo ""
-echo "Doit is set to: $doit"
-echo ""
-
-echo "Getting hosts from tailnet $tailnet..."
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "[Info]: Fetching hosts from tailnet $tailnet that have not been seen since: $remove_before_date"
+echo "[Info]: Currently 'doit' is set to: $doit"
+echo "[Info]: Getting hosts from tailnet: $tailnet"
 rm -f /tmp/tailscale-hosts
 curl -s "https://api.tailscale.com/api/v2/tailnet/$tailnet/devices" -u "$apikey:" | jq -r '.devices[] | "\(.lastSeen) \(.id) \(.hostname)"' >/tmp/tailscale-hosts
 
+echo "[Info]: Comparing hosts. Here are the results:"
 echo ""
 echo "Preserved:"
+echo "----------"
+echo ""
+printf "%-20s %-20s %-30s\n" "ID" "Last Seen" "Machine Name"
+printf "%-20s %-20s %-30s\n" "--------------------" "--------------------" "------------------------------"
 while IFS=' ' read -r seen id hostname; do
-  if [[ "$remove_before_date" < "$seen" ]]; then
-    echo "Hostname: $hostname - ID: $id was last seen $seen"
+  if command -v date >/dev/null; then
+    # Adjust for compatibility between GNU date and BSD date
+    if date --version >/dev/null 2>&1; then
+      # GNU date
+      remove_before_date_in_secs=$(date -d "$remove_before_date" "+%s")
+      seen_in_secs=$(date -d "$seen" "+%s")
+    else
+      # BSD date
+      remove_before_date_in_secs=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$remove_before_date" "+%s")
+      seen_in_secs=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$seen" "+%s")
+    fi
+  fi
+
+  if [[ "$remove_before_date_in_secs" -lt "$seen_in_secs" ]]; then
+    printf "%-20s %-20s %-30s\n" "$id" "$seen" "$hostname"
   fi
 done </tmp/tailscale-hosts
 
 echo ""
-echo ""
-echo ""
 echo "Removed:"
+echo "--------"
+echo ""
+printf "%-20s %-20s %-30s\n" "ID" "Last Seen" "Machine Name"
+printf "%-20s %-20s %-30s\n" "--------------------" "--------------------" "------------------------------"
 while IFS=' ' read -r seen id hostname; do
-  if [[ "$remove_before_date" > "$seen" ]]; then
-    echo "Hostname: $hostname - ID: $id was last seen $seen"
+  if command -v date >/dev/null; then
+    # Adjust for compatibility between GNU date and BSD date
+    if date --version >/dev/null 2>&1; then
+      # GNU date
+      remove_before_date_in_secs=$(date -d "$remove_before_date" "+%s")
+      seen_in_secs=$(date -d "$seen" "+%s")
+    else
+      # BSD date
+      remove_before_date_in_secs=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$remove_before_date" "+%s")
+      seen_in_secs=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$seen" "+%s")
+    fi
+  fi
+
+  if [[ "$remove_before_date_in_secs" -gt "$seen_in_secs" ]]; then
+    printf "%-20s %-20s %-30s\n" "$id" "$seen" "$hostname"
     if [[ $doit == "doit" ]]; then
+      # Corrected endpoint for device deletion to include tailnet
       curl -s -X DELETE "https://api.tailscale.com/api/v2/device/$id" -u "$apikey:"
     fi
   fi
 done </tmp/tailscale-hosts
 echo ""
-echo "Done"
+echo "[Info]: Done"
