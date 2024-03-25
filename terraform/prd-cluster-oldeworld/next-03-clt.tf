25df3d06bb486dd69e074d118cc84363244a0ca8
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-03-25 12:50:46 +0530

    fix(hcp): refactor to use new datasource

--- terraform/prd-cluster-oldeworld/next-03-clt.tf
+++ terraform/prd-cluster-oldeworld/next-03-clt.tf
@@ -30,7 +30,7 @@ resource "linode_instance_disk" "prd_oldeworld_clt_disk__boot" {
   linode_id = linode_instance.prd_oldeworld_clt[each.key].id
   size      = linode_instance.prd_oldeworld_clt[each.key].specs.0.disk
 
-  image     = data.hcp_packer_image.linode_ubuntu.cloud_image_id
+  image     = data.hcp_packer_artifact.linode_ubuntu.id
   root_pass = var.password
 
   stackscript_id = data.linode_stackscripts.cloudinit_scripts.stackscripts.0.id
--- terraform/prd-cluster-oldeworld/next-06-nws-jms.tf
+++ terraform/prd-cluster-oldeworld/next-06-nws-jms.tf
@@ -30,7 +30,7 @@ resource "linode_instance_disk" "prd_oldeworld_jms_disk__boot" {
   linode_id = linode_instance.prd_oldeworld_jms[count.index].id
   size      = linode_instance.prd_oldeworld_jms[count.index].specs.0.disk
 
-  image     = data.hcp_packer_image.linode_ubuntu.cloud_image_id
+  image     = data.hcp_packer_artifact.linode_ubuntu.id
   root_pass = var.password
 
   stackscript_id = data.linode_stackscripts.cloudinit_scripts.stackscripts.0.id
