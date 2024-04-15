026e7a2d0a6e8e62ac3dc0cffa308228ef4687ed
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-04-15 19:26:41 +0530

    feat(mintworld): add a network ELB and web proxy instances

--- terraform/stg-cluster-mintworld/main.tf
+++ terraform/stg-cluster-mintworld/main.tf
@@ -30,6 +30,7 @@ locals {
   count_svr_consul = 3
   count_svr_nomad  = 3
   count_wkr_nomad  = 5
+  count_web        = 3
 }
 
 # Private IPAM
@@ -38,6 +39,7 @@ locals {
   hostNum_start_svr_consul = 30 # Consul servers
   hostNum_start_svr_nomad  = 40 # Nomad servers
   hostNum_start_wkr_nomad  = 50 # Nomad workers
+  hostNum_start_web        = 10 # Web servers
 }
 
 locals {
@@ -158,3 +160,109 @@ module "stg_mw_nomad_wkr" {
     module.stg_mw_network
   ]
 }
+
+
+module "stg_mw_web" {
+  source = "./modules/instances"
+
+  instance_count  = local.count_web
+  instance_env    = "stg"
+  instance_prefix = "web"
+  instance_type   = "t3a.medium"
+
+  ami                  = data.hcp_packer_artifact.aws_ami.external_identifier
+  key_name             = data.aws_key_pair.stg_ssh_service_user_key.key_name
+  iam_instance_profile = data.aws_iam_instance_profile.stg_mw_instance_profile.name
+
+  security_group_ids = [
+    module.stg_mw_network.out__mw_sg_id
+  ]
+
+  hostNum_start = local.hostNum_start_web
+  subnets       = module.stg_mw_network.out__subnets.private
+
+  zone = {
+    name = local.zone
+    id   = data.cloudflare_zone.cf_zone.id
+  }
+
+  stack_tags = var.stack_tags
+
+  depends_on = [
+    module.stg_mw_network
+  ]
+}
+
+# resource "aws_lb_target_group" "stg_mw_web_tg__port_80" {
+#   name        = "stg-mw-web-tg--port-80"
+#   port        = 80
+#   protocol    = "HTTP"
+#   target_type = "instance"
+#   vpc_id      = module.stg_mw_network.out__vpc_id
+
+#   health_check {
+#     path                = "/"
+#     protocol            = "HTTP"
+#     timeout             = 5
+#     interval            = 30
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+
+#   tags = merge(var.stack_tags, {
+#     Name = "stg-mw-web-tg--port-80"
+#   })
+
+#   depends_on = [
+#     module.stg_mw_web
+#   ]
+# }
+
+# resource "aws_lb_target_group_attachment" "stg_mw_web_tga__port_80" {
+#   count = length(module.stg_mw_web.aws_instance)
+
+#   target_group_arn = aws_lb_target_group.stg_mw_web_tg__port_80.arn
+#   target_id        = module.stg_mw_web.aws_instance[count.index].id
+#   port             = 80
+
+#   depends_on = [
+#     module.stg_mw_web
+#   ]
+# }
+
+# resource "aws_lb_target_group" "stg_mw_web_tg__port_443" {
+#   name        = "stg-mw-web-tg--port-443"
+#   port        = 443
+#   protocol    = "HTTPS"
+#   target_type = "instance"
+#   vpc_id      = module.stg_mw_network.out__vpc_id
+
+#   health_check {
+#     path                = "/"
+#     protocol            = "HTTPS"
+#     timeout             = 5
+#     interval            = 30
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+
+#   tags = merge(var.stack_tags, {
+#     Name = "stg-mw-web-tg--port-443"
+#   })
+
+#   depends_on = [
+#     module.stg_mw_web
+#   ]
+# }
+
+# resource "aws_lb_target_group_attachment" "stg_mw_web_tga__port_443" {
+#   count = length(module.stg_mw_web.aws_instance)
+
+#   target_group_arn = aws_lb_target_group.stg_mw_web_tg__port_443.arn
+#   target_id        = module.stg_mw_web.aws_instance[count.index].id
+#   port             = 443
+
+#   depends_on = [
+#     module.stg_mw_web
+#   ]
+# }
--- terraform/stg-cluster-mintworld/modules/network/outputs.tf
+++ terraform/stg-cluster-mintworld/modules/network/outputs.tf
@@ -5,6 +5,11 @@ output "out__mw_sg_id" {
   description = "Security group for the Staging Mintworld VPC"
 }
 
+output "out__mw_sg_web_id" {
+  value       = aws_security_group.mw_sg_web.id
+  description = "Security group for the Staging Mintworld VPC for Web Proxy"
+}
+
 output "out__subnets" {
   value = {
     private = [for subnet in aws_subnet.mw_subnet_prv : {
@@ -18,4 +23,9 @@ output "out__subnets" {
       cidr_block = subnet.cidr_block,
     }]
   }
+}
+
+output "out__vpc_id" {
+  value       = aws_vpc.mw_vpc.id
+  description = "VPC for the Staging Mintworld VPC"
 }
