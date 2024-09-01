6ee1b8d66ec9367c7efda6bbb7262b0037f397a4
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-09-01 20:48:00 +0530

    feat(linode): drop MySQL 5.7 cluster

--- terraform/prd-cluster-oldeworld-db/main.tf
+++ terraform/prd-cluster-oldeworld-db/main.tf
@@ -23,29 +23,6 @@ data "linode_instances" "prd_oldeworld_nws" {
   }
 }
 
-resource "linode_database_mysql" "prd_oldeworld_nws_db__mysql57" {
-  engine_id = "mysql/5.7.39"
-  label     = "prd-db-oldeworld-nws-mysql57"
-  region    = var.region
-  type      = "g6-dedicated-4"
-
-  allow_list = flatten([
-    [for i in data.linode_instances.prd_oldeworld_nws.instances : "${i.private_ip_address}/32"]
-  ])
-
-  cluster_size     = 3
-  replication_type = "asynch"
-  ssl_connection   = true
-
-  updates {
-    day_of_week   = "sunday"
-    duration      = 1
-    frequency     = "monthly"
-    hour_of_day   = 2
-    week_of_month = 2
-  }
-}
-
 resource "linode_database_mysql" "prd_oldeworld_nws_db__mysql80" {
   engine_id = "mysql/8.0.30"
   label     = "prd-db-oldeworld-nws-mysql80"
